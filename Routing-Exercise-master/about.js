df61605a9a5c0cf609ff17f883983106f6a360cf
VishalChhabra96 <CHHABRA.VISHAL96@GMAIL.COM>
2018-12-16 22:39:21 +0530

    First Commit

---
+++ Routing-Exercise-master/about.js
@@ -0,0 +1,8 @@
+import React, {Component } from "react";
+import "./route.css";
+const About=()=>{
+    return(
+        <h2 style={{color:"green"}}>Public Route ====>>>>  About Page</h2>
+    )
+}
+export default About;
